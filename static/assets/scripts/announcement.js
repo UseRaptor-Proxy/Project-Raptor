72f6617eb720bba92e100fe3180307285936a5cd
JackGrounds <134026432+JackGrounds@users.noreply.github.com>
2024-06-10 14:28:10 -0400

    update

--- static/assets/scripts/announcement.js
+++ static/assets/scripts/announcement.js
@@ -1,17 +1,17 @@
-var announcement = true 
+var announcement = true;
 
 if (announcement) {
-  var announcementElement = document.getElementById("announcement") 
+  var announcementElement = document.getElementById("announcement");
 
-  var h1 = document.createElement("h1") 
-  h1.innerHTML = "Announcement: We are working on adding more proxies" 
-  announcementElement.appendChild(h1) 
+  var h1 = document.createElement("h1");
+  h1.innerHTML = "Announcement: Welcome";
+  announcementElement.appendChild(h1);
 } else {
-  var navbar = document.getElementsByClassName("fixed-nav-bar") 
+  var navbar = document.getElementsByClassName("fixed-nav-bar");
   if (navbar.length > 0) {
-    navbar = document.getElementsByClassName("fixed-nav-bar")[0] 
-    navbar.style = "top: 0  margin-bottom: 2vh " 
+    navbar = document.getElementsByClassName("fixed-nav-bar")[0];
+    navbar.style = "top: 0  margin-bottom: 2vh ";
   } else {
-    print("No navbar!") 
+    print("No navbar!");
   }
 }
